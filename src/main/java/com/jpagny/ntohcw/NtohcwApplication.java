9c2f15c4c9d4e07f2dee61384ac34e03ea7583c2
Jérôme Pagny <pagny.jerome@gmail.com>
2022-10-11 17:50:13 +0200

    Initialize springboot

---
+++ src/main/java/com/jpagny/ntohcw/NtohcwApplication.java
@@ -0,0 +1,13 @@
+package com.jpagny.ntohcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtohcwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtohcwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpagny/ntohcw/NtohcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpagny.ntohcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtohcwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
